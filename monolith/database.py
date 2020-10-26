3231275a82541ae401430fdf899fac0e696fb6be
OrnelaDanushi <50953998+OrnelaDanushi@users.noreply.github.com>
2020-10-26 23:20:54 +0100

    Update database.py

--- monolith/database.py
+++ monolith/database.py
@@ -19,13 +19,12 @@ class User(db.Model):
     email = db.Column(db.Unicode(128), nullable=False, unique=True)
     firstname = db.Column(db.Unicode(128))
     lastname = db.Column(db.Unicode(128))
-    password = db.Column(db.Unicode(128), nullable=False)
+    password = db.Column(db.Unicode(128), nullable=False) 
     dateofbirth = db.Column(db.DateTime)
     is_active = db.Column(db.Boolean, default=True)
     is_admin = db.Column(db.Boolean, default=False)
     is_anonymous = False
-
-    # role = db.Column(db.Unicode(128)) # 0=customer, 1=rest_owner, 2=Asl
+    role = db.Column(db.Unicode(128)) # 0=customer, 1=rest_owner, 2=Asl
 
     def __init__(self, *args, **kw):
         super(User, self).__init__(*args, **kw)
@@ -40,21 +39,22 @@ class User(db.Model):
 
     def authenticate(self, password):
         checked = check_password_hash(self.password, password)
-        self._authenticated = checked
+        self._authenticated = checked  # it is true if the user password is correct
         return self._authenticated
 
     def get_id(self):
         return self.id
 
-    # __table_args__ = (CheckConstraint(len ( password ) >= 8, name='check_length_password'), {})
+
+
 
 
 class Restaurant(db.Model):
     __tablename__ = 'restaurant'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 
-    # owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    # owner = relationship('User', foreign_keys='Restaurant.owner_id')
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    owner = relationship('User', foreign_keys='Restaurant.owner_id')
 
     name = db.Column(db.Text(100))
     likes = db.Column(db.Integer)  # will store the number of likes, periodically updated in background
@@ -72,13 +72,12 @@ class Restaurant(db.Model):
 
 
 
-
 class Table(db.Model):
     __tablename__ = 'table'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 
     restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'))
-    restaurant = relationship('Restaurant', foreign_keys='Table.restaurant_id')  # TO CHECK if it is really used
+    restaurant = relationship('Restaurant', foreign_keys='Table.restaurant_id') 
 
     name = db.Column(db.Text(100))    
     capacity = db.Column(db.Integer)  # avoid neg numbers
@@ -89,11 +88,10 @@ class WorkingDay(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 
     restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'))
-    restaurant = relationship('Restaurant', foreign_keys='WorkingDay.restaurant_id')  # TO CHECK if it is really used
+    restaurant = relationship('Restaurant', foreign_keys='WorkingDay.restaurant_id')  
 
-    work_shifts = db.Column(db.Enum)  # or is better to serialize a python object?
-    day = db.Column(db.Integer, nullable=False)  # 0=Mon, 1=Tue, ...
-
+    work_shifts = db.Column(db.PickleType)  
+    day = db.Column(db.Integer, db.CheckConstraint('day>=0' and 'day<=6'), nullable=False)  # 0=Mon, 1=Tue, ...
 
 
 # is like a pretty rating implementation
@@ -107,3 +105,72 @@ class Like(db.Model):
     restaurant = relationship('Restaurant', foreign_keys='Like.restaurant_id')
 
     marked = db.Column(db.Boolean, default=False)  # True iff it has been counted in Restaurant.likes
+
+
+class Reservation(db.Model):
+    __tablename__ = 'reservation'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+    booker_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    booker = relationship('User', foreign_keys='Reservation.booker_id')
+
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'))
+    restaurant = relationship('Restaurant', foreign_keys='Reservation.restaurant_id')
+
+    table_id = db.Column(db.Integer, db.ForeignKey('table.id'))
+    table = relationship('Table', foreign_keys='Reservation.table_id')
+
+    date = db.Column(db.DateTime)
+    hour = db.Column(db.PickleType)
+    cancelled = db.Column(db.Boolean, default=True)
+
+
+class Seat(db.Model):
+    __tablename__ = 'seat'
+
+    reservation_id = db.Column(db.Integer, db.ForeignKey('reservation.id'), primary_key=True)
+    reservation = relationship('Reservation', foreign_keys='Seat.reservation_id')
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    user= relationship('User', foreign_keys='Seat.user_id')
+
+    confirmed = db.Column(db.Boolean, default=True)
+
+
+class Review(db.Model):
+    __tablename__ = 'review'
+
+    reviewer_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    reviewer = relationship('User', foreign_keys='Review.reviewer_id')
+
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), primary_key=True)
+    restaurant = relationship('Restaurant', foreign_keys='Review.restaurant_id')
+
+    marked = db.Column(db.Boolean, default=True)
+    rating = db.Column(db.Integer)
+    comment = db.Column(db.Text(200))
+    date = db.Column(db.DateTime)
+
+class Photo(db.Model):
+    __tablename__ = 'photo'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'))
+    restaurant = relationship('Restaurant', foreign_keys='Photo.restaurant_id')
+
+    path = db.Column(db.Unicode(128))
+    description = db.Column(db.Unicode(128))
+
+
+class Dishes(db.Model):
+    __tablename__ = 'dishes'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'))
+    restaurant = relationship('Restaurant', foreign_keys='Dishes.restaurant_id')
+
+    price = db.Column(db.Float)
+    name = db.Column(db.Unicode(128))
+    ingredients = db.Column(db.PickleType)
+
+

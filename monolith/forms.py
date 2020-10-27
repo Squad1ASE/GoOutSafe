559d3b4eb0b575b407dc6e2a8c2ad2f3aa2a5c22
OrnelaDanushi <50953998+OrnelaDanushi@users.noreply.github.com>
2020-10-27 20:53:42 +0100

    Update forms.py

--- monolith/forms.py
+++ monolith/forms.py
@@ -1,29 +1,20 @@
 from flask_wtf import FlaskForm
 import wtforms as f
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length, Email
 
 
 class LoginForm(FlaskForm):
-    email = f.StringField('email', validators=[DataRequired()])
-    password = f.PasswordField('password', validators=[DataRequired()])
+    email = f.StringField('email', validators=[DataRequired(), Length(1, 64), Email()])    
+    password = f.PasswordField('password', validators=[DataRequired(), Length(1,8)])
     display = ['email', 'password']
 
 
 class UserForm(FlaskForm):
-    email = f.StringField('email', validators=[DataRequired()])
+    email = f.StringField('email', validators=[DataRequired(), Length(1, 64), Email()])    
     firstname = f.StringField('firstname', validators=[DataRequired()])
     lastname = f.StringField('lastname', validators=[DataRequired()])
-    password = f.PasswordField('password', validators=[DataRequired()])
+    password = f.PasswordField('password', validators=[DataRequired(), Length(1,8)])
     dateofbirth = f.DateField('dateofbirth', format='%d/%m/%Y')
     display = ['email', 'firstname', 'lastname', 'password', 'dateofbirth']
 
-class RestaurantForm(FlaskForm):
-    name = f.StringField('name', validators=[DataRequired()])
-    lat = f.StringField('lat', validators=[DataRequired()])
-    lon = f.StringField('lon', validators=[DataRequired()])
-    phone = f.StringField('phone', validators=[DataRequired()])
-    # maybe owner has to complete the tables descritions and capacity is 
-    # automatically derived from tables settings
-    capacity = f.StringField('capacity', validators=[DataRequired()])
-    prec_measures = f.StringField('prec_measures', validators=[DataRequired()])
-    display = ['name', 'lat', 'lon', 'phone', 'capacity','prec_measures']
+

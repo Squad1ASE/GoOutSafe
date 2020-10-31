cc79f2520d40b86860be2e576eec73db86d8c214
Ornela <orneladanushi@gmail.com>
2020-10-31 15:03:06 +0100

    updating db with the new version of general_view

--- monolith/database.py
+++ monolith/database.py
@@ -49,7 +49,6 @@ class User(db.Model):
 
 
 
-
 class Restaurant(db.Model):
     __tablename__ = 'restaurant'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -57,7 +56,7 @@ class Restaurant(db.Model):
     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     owner = relationship('User', foreign_keys='Restaurant.owner_id')
 
-    name = db.Column(db.Text(100), nullable=False)
+    name = db.Column(db.Unicode(128), nullable=False)
     likes = db.Column(db.Integer, db.CheckConstraint('likes>=0'), default=0)  # will store the number of likes, periodically updated in background
     lat = db.Column(db.Float, nullable=False)  # restaurant latitude
     lon = db.Column(db.Float, nullable=False)  # restaurant longitude
@@ -67,10 +66,10 @@ class Restaurant(db.Model):
 
     cuisine_type = db.Column(db.PickleType, nullable=False)
 
-    prec_measures = db.Column(db.Text(200), nullable=False)
+    prec_measures = db.Column(db.Unicode(128), nullable=False)
     tot_reviews = db.Column(db.Integer, db.CheckConstraint('tot_reviews>=0'), default=0)
     avg_rating = db.Column(db.Float, db.CheckConstraint('avg_rating>=0' and 'avg_rating<=5'), default=0)
-
+    avg_time_of_stay = db.Column(db.Float, db.CheckConstraint('avg_time_of_stay>=0' and 'avg_time_of_stay<=5'), default=0)
 
 
 class Table(db.Model):
@@ -80,8 +79,8 @@ class Table(db.Model):
     restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'))
     restaurant = relationship('Restaurant', foreign_keys='Table.restaurant_id') 
 
-    name = db.Column(db.Text(100))    
-    capacity = db.Column(db.Integer)  # avoid neg numbers
+    name = db.Column(db.Unicode(128))    
+    capacity = db.Column(db.Integer, db.CheckConstraint('capacity>=0'))  # avoid neg numbers
 
 
 class WorkingDay(db.Model):
@@ -95,6 +94,7 @@ class WorkingDay(db.Model):
 
     day = db.Column(db.Integer, db.CheckConstraint('day>=0' and 'day<=6'), nullable=False)  # 0=Mon, 1=Tue, ...
     # the constraint is on the value of the column
+
 
 
 # is like a pretty rating implementation
@@ -128,18 +128,17 @@ class Reservation(db.Model):
     cancelled = db.Column(db.Boolean, default=True)
 
 
+
 class Seat(db.Model):
     __tablename__ = 'seat'
 
     reservation_id = db.Column(db.Integer, db.ForeignKey('reservation.id'), primary_key=True)
     reservation = relationship('Reservation', foreign_keys='Seat.reservation_id')
 
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
-    user= relationship('User', foreign_keys='Seat.user_id')
-
-    guests_email = db.Column(db.Unicode(128))
+    guests_email = db.Column(db.String, nullable=False, unique=True)  
 
     confirmed = db.Column(db.Boolean, default=True)
+
 
 
 class Review(db.Model):
@@ -153,8 +152,9 @@ class Review(db.Model):
 
     marked = db.Column(db.Boolean, default=True)
     rating = db.Column(db.Integer)
-    comment = db.Column(db.Text(200))
+    comment = db.Column(db.Unicode(128))
     date = db.Column(db.DateTime)
+
 
 class Photo(db.Model):
     __tablename__ = 'photo'
@@ -179,15 +179,6 @@ class Dishes(db.Model):
     ingredients = db.Column(db.PickleType)
 
 
-class ReportOfPositivity(db.Model):
-    __tablename__  = 'report_of_positivity'
-
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
-    user = relationship('User', foreign_keys='ReportOfPositivity.user_id')
-
-    date = db.Column(db.DateTime)
-
-
 class Quarantine(db.Model):
     __tablename__ = 'quarantine'
 
@@ -197,7 +188,7 @@ class Quarantine(db.Model):
     start_date = db.Column(db.DateTime)
     end_date = db.Column(db.DateTime)
 
-    active = db.Column(db.Boolean, default=True)
+    in_observation = db.Column(db.Boolean, default=True) #True=can't book
 
 
 class Notification(db.Model):
@@ -208,7 +199,6 @@ class Notification(db.Model):
 
     message = db.Column(db.Unicode(128))
     pending = db.Column(db.Boolean, default=True)
-    type_ = db.Column(db.Integer)  # 0=through email, 1=phone, 2=app
+    type_ = db.Column(db.Integer)  # 0=through email, 2=app
     date = db.Column(db.DateTime)
 
-#comment
